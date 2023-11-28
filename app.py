4c8676cecc8651d06a91fda939f9404a9c13a102
Diwakarkothari <111675672+Diwakarkothari@users.noreply.github.com>
2023-11-28 14:07:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -284,7 +284,7 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
 
 # docker build -t flask:latest .
 # docker run -i -p 5000:5000 -d flask
